bd70836b5af3bc916b395793c015e31d8c051153
InnocentEdem <80456476+Quessi@users.noreply.github.com>
2023-08-15 10:40:16 +0000

    Resolve python issues

--- libs/core/enums.d.ts
+++ libs/core/enums.d.ts
@@ -5,6 +5,8 @@ declare const enum Direction {
   Right,
 }
 
+
+
 declare const enum Hop {
   //% block=short
   Short,
@@ -96,3 +98,23 @@ Purple=6,
 White=7,
 Black=8,
 }
+
+declare const enum NumberFormat {
+  Int8LE = 1,
+  UInt8LE = 2,
+  Int16LE = 3,
+  UInt16LE = 4,
+  Int32LE = 5,
+  Int8BE = 6,
+  UInt8BE = 7,
+  Int16BE = 8,
+  UInt16BE = 9,
+  Int32BE = 10,
+
+  UInt32LE = 11,
+  UInt32BE = 12,
+  Float32LE = 13,
+  Float64LE = 14,
+  Float32BE = 15,
+  Float64BE = 16,
+  }
