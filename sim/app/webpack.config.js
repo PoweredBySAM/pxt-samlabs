c74ea8c822ee28d0ae30cce1fb2cb24a59022956
InnocentEdem <80456476+Quessi@users.noreply.github.com>
2023-08-31 18:05:41 +0000

    update webpack mode

--- sim/app/webpack.config.js
+++ sim/app/webpack.config.js
@@ -57,9 +57,3 @@ module.exports = {
   },
 };
 
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'src/index.html',
-//       inject: false, // Set to false to manually inject the bundle
-//     }),
-//   ],
