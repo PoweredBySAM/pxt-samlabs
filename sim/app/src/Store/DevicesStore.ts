7f92aab034d933e8c7cdee1f0323a68a4873ab56
k00ry <koory.yellow@gmail.com>
2025-04-08 09:47:47 -0500

    feat/SAMScript iPad implementation

--- sim/app/src/Store/DevicesStore.ts
+++ sim/app/src/Store/DevicesStore.ts
@@ -1,82 +1,97 @@
-import type { IBuiltDevice } from "../SAMDevices/Types/SAMDeviceTypes";
-import { observable, action, makeAutoObservable } from "mobx";
+import type {IBuiltDevice} from '../SAMDevices/Types/SAMDeviceTypes';
+import {observable, action, makeAutoObservable} from 'mobx';
 
-import ButtonDevice from "./ButtonDevice";
-import BuzzerDevice from "./BuzzerDevice";
-import DCMotorDevice from "./DCMotorDevice";
-import HeatSensorDevice from "./HeatSensorDevice";
-import LEDDevice from "./LEDDevice";
-import LightSensorDevice from "./LightSensorDevice";
-import MicrobitDevice from "./MicrobitDevice";
-import PressureSensorDevice from "./PressureSensorDevice";
-import ProximitySensorDevice from "./ProximitySensorDevice";
-import ServoMotorDevice from "./ServoMotorDevice";
-import SliderDevice from "./SliderDevice";
-import TiltDevice from "./TiltDevice";
-import SamDeviceManager from "src/Features/SamSimState";
+import ButtonDevice from './ButtonDevice';
+import BuzzerDevice from './BuzzerDevice';
+import DCMotorDevice from './DCMotorDevice';
+import HeatSensorDevice from './HeatSensorDevice';
+import LEDDevice from './LEDDevice';
+import LightSensorDevice from './LightSensorDevice';
+import MicrobitDevice from './MicrobitDevice';
+import PressureSensorDevice from './PressureSensorDevice';
+import ProximitySensorDevice from './ProximitySensorDevice';
+import ServoMotorDevice from './ServoMotorDevice';
+import SliderDevice from './SliderDevice';
+import TiltDevice from './TiltDevice';
+import SamDeviceManager from 'src/Features/SamSimState';
 
 export const storeMap = {
-  "SAM Button": ButtonDevice,
-  "SAM Buzzer": BuzzerDevice,
-  "SAM RGB Light": LEDDevice,
-  "SAM DC Motor": DCMotorDevice,
-  "SAM Light Sensor": LightSensorDevice,
-  "SAM Servo Motor": ServoMotorDevice,
-  "SAM Pressure Sensor": PressureSensorDevice,
-  "SAM Proximity Sensor": ProximitySensorDevice,
-  "SAM Slider": SliderDevice,
-  "SAM Heat Sensor": HeatSensorDevice,
-  "SAM Tilt": TiltDevice,
-  "BBC Microbit": MicrobitDevice,
+    'SAM Button': ButtonDevice,
+    'SAM Buzzer': BuzzerDevice,
+    'SAM RGB Light': LEDDevice,
+    'SAM DC Motor': DCMotorDevice,
+    'SAM Light Sensor': LightSensorDevice,
+    'SAM Servo Motor': ServoMotorDevice,
+    'SAM Pressure Sensor': PressureSensorDevice,
+    'SAM Proximity Sensor': ProximitySensorDevice,
+    'SAM Slider': SliderDevice,
+    'SAM Heat Sensor': HeatSensorDevice,
+    'SAM Tilt': TiltDevice,
+    'BBC Microbit': MicrobitDevice,
 };
 
 class DevicesStore {
-  @observable devices: any[] = [];
-  lsStateStore: any;
-  constructor() {
-    makeAutoObservable(this);
-    this.lsStateStore = SamDeviceManager.getInstance();
-  }
+    @observable devices: any[] = [];
+    lsStateStore: any;
+    private deviceJustAdded: boolean = false;
 
-  @action addDevice(deviceData: IBuiltDevice) {
-    const device = this.buildStore(deviceData);
-    this.devices.push(device);
-    this.updateAssignedNames();
-    this.devices.forEach((device) => {
-      window.parent.postMessage(
-        {
-          type: device.createMessageType,
-          id: device.assignedName,
-        },
-        window.location.origin
-      );
-    });
-  }
-
-  @action emptyDevicesStore() {
-    this.devices = [];
-    this.lsStateStore.emptySamSimState();
-  }
-  @action updateAssignedNames() {
-    const counts: { [key: string]: number } = {};
-
-    this.devices.forEach((device) => {
-      const deviceType = device.constructor.name;
-      const count = counts[deviceType] || 0;
-      counts[deviceType] = count + 1;
-      device.assignedName = `${deviceType} ${count === 0 ? '' : count + 1}`;
-    });
-  }
-
-  buildStore(deviceData: IBuiltDevice) {
-    const store =
-      storeMap[deviceData.labels.defaultName as keyof typeof storeMap];
-    if (store) {
-      return new store(deviceData);
+    constructor() {
+        makeAutoObservable(this);
+        this.lsStateStore = SamDeviceManager.getInstance();
     }
-    throw new Error("No store in storemap for device");
-  }
+
+    @action addDevice(deviceData: IBuiltDevice) {
+        const device = this.buildStore(deviceData);
+
+        this.devices = [...this.devices, device];
+
+        this.updateAssignedNames();
+
+        this.deviceJustAdded = true;
+
+        setTimeout(() => {
+            this.deviceJustAdded = false;
+        }, 500);
+
+        this.devices.forEach((device) => {
+            window.parent.postMessage(
+                {
+                    type: device.createMessageType,
+                    id: device.assignedName,
+                },
+                window.location.origin
+            );
+        });
+    }
+
+    @action emptyDevicesStore() {
+        if (this.deviceJustAdded) {
+            return;
+        }
+
+        this.devices = [];
+        this.lsStateStore.emptySamSimState();
+    }
+    @action updateAssignedNames() {
+        const counts: {[key: string]: number} = {};
+
+        this.devices.forEach((device) => {
+            const deviceType = device.constructor.name;
+            const count = counts[deviceType] || 0;
+            counts[deviceType] = count + 1;
+            device.assignedName = `${deviceType} ${count === 0 ? '' : count + 1}`;
+        });
+    }
+
+    buildStore(deviceData: IBuiltDevice) {
+        const store = storeMap[deviceData.labels.defaultName as keyof typeof storeMap];
+        if (store) {
+            return new store(deviceData);
+        }
+        throw new Error('No store in storemap for device');
+    }
 }
+
 const devicesStore = new DevicesStore();
 
 export default devicesStore;
